e47459d6eb715a48fe47960eabcea21a0d8cfeb3
Graham Kim <graham.kim@cantab.net>
2020-07-27 10:23:34 +0100

    Issue #5 New FormationManager to handle adding nodes and links
    For now, does nothing new.

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ from canvas import Canvas
 import model
 
 if __name__ == '__main__':
-    nodes = [
-        model.Node("abc", (400,300), "red"),
-        model.Node("def", (300,200), "green")
-    ]
+    mgr = model.FormationManager()
+    n1 = mgr.add_node("abc", (400,300), "red")
+    n2 = mgr.add_node("def", (300,200), "green")
+    mgr.add_link(n1, n2)
 
-    links = [
-        model.Link(nodes[0], nodes[1])
-    ]
-
-    c = Canvas(nodes, links)
+    c = Canvas(mgr.nodes, mgr.links)
     c.main_loop()
--- model.py
+++ model.py
@@ -7,6 +7,29 @@ class Node:
         self.colour = colour
 
 class Link:
-    def __init__(self, from_node: Node, to_node: Node):
-        self.from_model_node_id = id(from_node)
-        self.to_model_node_id = id(to_node)
+    def __init__(self, from_id: int, to_id: int):
+        self.from_model_node_id = from_id
+        self.to_model_node_id = to_id
+
+class FormationManager:
+    def __init__(self):
+        self._nodes = {}
+        self._links = []
+
+    @property
+    def nodes(self) -> tp.List[Node]:
+        return [n for n in self._nodes.values()]
+
+    @property
+    def links(self) -> tp.List[Link]:
+        return self._links
+
+    def add_node(self, text: str, pos: tp.Tuple[int, int], colour: str) -> int:
+        new_node = Node(text, pos, colour)
+        new_id = id(new_node)
+        self._nodes[new_id] = new_node
+        return new_id
+
+    def add_link(self, from_id: int, to_id: int):
+        self._links.append( Link(from_id, to_id) )
+
