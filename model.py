e8d6254b342ea0194dc9f090e3eed470681fab90
Graham Kim <graham.kim@cantab.net>
2020-08-16 11:39:55 +0100

    Issue #32 model::Link now handles BACK_ARROW

--- model.py
+++ model.py
@@ -16,10 +16,16 @@ class Node:
 
 class Link:
     def __init__(self, from_id: int, to_id: int, colour: str, arrow_draw: ArrowDraw, second_colour: tp.Optional[str] = None):
-        self.from_model_node_id = from_id
-        self.to_model_node_id = to_id
+        if arrow_draw == ArrowDraw.BACK_ARROW:
+            self.from_model_node_id = to_id
+            self.to_model_node_id = from_id
+            self.arrow_draw = ArrowDraw.FWD_ARROW
+        else:
+            self.from_model_node_id = from_id
+            self.to_model_node_id = to_id
+            self.arrow_draw = arrow_draw
+
         self.colour = colour
-        self.arrow_draw = arrow_draw
         self.second_colour = second_colour # for DUAL_LINK
 
         if second_colour is not None and arrow_draw != ArrowDraw.DUAL_LINK:
